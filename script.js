980d2fd5911585999ab49f17dbedca08cb7346d5
hamed <hamed.bakhshi7809@gmail.com>
2024-02-23 00:44:19 +0100

    EventListener to get users data

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const container = document.querySelector('.container')
+//declare all the inputs
+const container = document.querySelector('.book-container')
 const openDia = document.querySelector('.add-book')
 const dialog = document.querySelector('dialog')
 const closeDia = document.querySelector('.close-dialog')
+const form = document.querySelector('form')
 const myLibrary = [];
-
+//constructor function
 function Book(title, author, pages, read) {
   this.title = title
   this.author = author
   this.pages = pages
   this.read = read
   }
-
+//function to add the book into the array
 function addBookToLibrary(title, author, pages, read) {
 let newBook = new Book(title, author, pages, read)
 myLibrary.push(newBook)
 }
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '295 pages', false)
-console.log(myLibrary);
 
+//Prints the books into HTMl
 for (let i = 0; i < myLibrary.length; i++){
  let card = document.createElement('div')
  let paraG = document.createElement('p')
@@ -33,3 +35,13 @@ else{
 openDia.addEventListener('click', () => {
   dialog.showModal()
 })
+
+//Get user input 
+
+form.addEventListener('submit', callbackFunction);
+function callbackFunction(event) {
+  event.preventDefault();
+  const myFormData = new FormData(event.target);
+
+  const formDataObj = Object.fromEntries(myFormData.entries());
+  console.log(formDataObj);}
